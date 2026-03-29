fd727afa0183f056b71fc3b05dd3a3c2185891d8
FilinStudioUA <volchkov.yuraa@gmail.com>
2026-03-29 23:43:01 +0300

    Delete online_courses directory

--- online_courses/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///courses.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- online_courses/app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-migrate = Migrate()
-login_manager = LoginManager()
-login_manager.login_view = "auth.login"  # якщо буде blueprint auth
